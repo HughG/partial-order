78fb3f8b5da22602f05da9bfba43c9a99969fa5c
Hugh Greene <githugh@tameter.org>
2016-03-28 23:16:55 +0100

    Just have a native class, instead of interface plus class, now that I'vw worked out how native constructors work.

--- kotlin-test-app/src/org/tameter/kotlin-test-app.kt
+++ kotlin-test-app/src/org/tameter/kotlin-test-app.kt
@@ -12,26 +12,19 @@ interface Promise {
     fun catch(error: (dynamic) -> Unit): Unit
 }
 
+@Suppress("unused")
 @native
-interface PouchDB {
-    @native fun info(): Promise = noImpl
+class PouchDB(var name: String, var options: Map<String, dynamic>)
+{
+    constructor(name: String) : this(name, mapOf())
+    fun info(): Promise = noImpl
 }
 
-@native("PouchDB")
-class PDB(var name: String): PouchDB {}
-
-@native val db: PouchDB
-
 fun main(args: Array<String>) {
-    console.log("Setting email ...")
-    document.getElementById("email")?.setAttribute("value", "hello@kotlinlang.org")
-    console.log("  ... done")
+//    console.log("Setting email ...")
+//    document.getElementById("email")?.setAttribute("value", "hello@kotlinlang.org")
 
-    js("var db = new PouchDB('http://localhost:5984/kittens')")
-//    js("var db = new PouchDB('kittens')")
-
-    val db2: PDB = PDB("http://localhost:5984/rats")
+    val db: PouchDB = PouchDB("http://localhost:5984/rats")
 
     db.info().then({ console.log(it) })
-    db2.info().then({ console.log(it) })
 }
