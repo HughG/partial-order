4f9744add7f68e4a31ac0cb1b929d77cbc626dc5
Hugh Greene <githugh@tameter.org>
2016-03-29 11:55:23 +0100

    Various attempts at creating native JS objects.

--- kotlin-test-app/src/org/tameter/kotlin-test-app.kt
+++ kotlin-test-app/src/org/tameter/kotlin-test-app.kt
@@ -7,18 +7,54 @@ import kotlin.browser.document
  */
 
 @native
-interface Promise {
-    fun then(result: (dynamic) -> dynamic): Promise
-    fun catch(error: (dynamic) -> Unit): Unit
+interface Promise<T> {
+//    fun then(result: (T) -> dynamic): dynamic
+    fun then(result: (T) -> T): Promise<T>
+    fun catch(error: (T) -> Unit): Unit
 }
+
+@native
+interface Promise0 : Promise<dynamic> {}
 
 @Suppress("unused")
 @native
 class PouchDB(var name: String, var options: Map<String, dynamic>)
 {
     constructor(name: String) : this(name, mapOf())
-    fun info(): Promise = noImpl
+    fun info(): Promise0 = noImpl
+    fun put(doc: dynamic): Promise0 = noImpl
+    fun get(id: String): Promise0 = noImpl
 }
+
+@native
+open class Object {
+    @nativeGetter
+    fun get(prop: String): dynamic = noImpl
+
+    @nativeSetter
+    fun set(prop: String, value: dynamic) {}
+}
+
+@native("Object")
+open class PouchDoc(val _id: String = noImpl) : Object() {}
+
+@native("Object")
+class Kitten(
+        _id: String,
+        var name: String = noImpl,
+        var occupation: String = noImpl,
+        var age: Int = noImpl,
+        var hobbies: Array<String> = noImpl
+) : PouchDoc(_id) {
+
+}
+
+fun <T: Object> initObject(obj: T, init: T.() -> Unit): T {
+    obj.init()
+    return obj
+}
+
+fun kitten(_id: String, init: Kitten.() -> Unit): Kitten = initObject(Kitten(_id), init)
 
 fun main(args: Array<String>) {
 //    console.log("Setting email ...")
@@ -27,4 +63,43 @@ fun main(args: Array<String>) {
     val db: PouchDB = PouchDB("http://localhost:5984/rats")
 
     db.info().then({ console.log(it) })
+
+    var doc = kitten("mittens") {
+//        _id = "mittens",
+        name = "Mittens";
+        occupation = "kitten";
+        age = 3;
+        hobbies = arrayOf(
+                "playing with balls of yarn",
+                "chasing laser pointers",
+                "lookin' hella cute"
+        );
+    }
+
+//    var doc: dynamic;
+//    doc._id = "mittens",
+//            name = "Mittens",
+//            occupation = "kitten",
+//            age = 3,
+//            hobbies = arrayOf(
+//                    "playing with balls of yarn",
+//                    "chasing laser pointers",
+//                    "lookin' hella cute"
+//            )
+//    )
+    console.log(doc)
+    db.put(doc)
+
+//    db.get("mittens").then(fun(doc): dynamic {
+//        // update their age
+//        doc.age = 4
+//        // put them back
+//        return db.put(doc)
+//    }).then(fun(doc): dynamic {
+//        // fetch mittens again
+//        return db.get("mittens")
+//    }).then(fun(doc): dynamic {
+//        console.log(doc);
+//        return null;
+//    });
 }
