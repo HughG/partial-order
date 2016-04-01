13ffcbb4cb9853876f34816d7953e89b99b9bf4a
Hugh Greene <githugh@tameter.org>
2016-04-01 11:19:33 +0100

    Fixes and cleanups.
    * Fix the fact that I wasn't threading everything through promises at DB init.
    * Use bulkDocs for init, and add StoreResult.
    * Shorten doc IDs.
    * Remove useless-in-JS "init{}" blocks.
    * Remove unused DOM code.

--- kotlin-test-app/src/org/tameter/kotlin-test-app.kt
+++ kotlin-test-app/src/org/tameter/kotlin-test-app.kt
@@ -1,7 +1,5 @@
 package org.tameter
 
-import kotlin.browser.document
-
 /**
  * Created by hughg on 2016-03-28.
  */
@@ -10,8 +8,9 @@ import kotlin.browser.document
     See very useful info about (newbie mistakes with) promises at
 
     https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html
- */
 
+    TODO 2016-04-01 HughG: Add @CheckReturnValue from FundBugs / JSR 305
+ */
 @native
 abstract class Promise<T> {
     @native("then") fun <U> thenV(result: (T) -> U): Promise<U>
@@ -21,6 +20,13 @@ abstract class Promise<T> {
 
 fun <T> Promise<T>.catchAndLog(): Unit {
     catch { console.log(it) }
+}
+
+@native
+interface StoreResult {
+    val ok: Boolean
+    val id: String
+    val rev: String
 }
 
 @native
@@ -52,11 +58,11 @@ open class PouchDB(var name: String, var options: JSMap<dynamic> = JSMap())
     fun destroy(options: JSMap<dynamic> = JSMap()): Promise<dynamic> = noImpl
 
     // Create/update doc
-    fun put(doc: dynamic): Promise<dynamic> = noImpl
+    fun put(doc: dynamic): Promise<StoreResult> = noImpl
     fun get(id: String): Promise<dynamic> = noImpl
 
     // Batch create
-    fun bulkDocs(docs: Array<Any>, options: JSMap<dynamic> = JSMap()): Promise<dynamic> = noImpl
+    fun bulkDocs(docs: Array<Any>, options: JSMap<dynamic> = JSMap()): Promise<Array<StoreResult>> = noImpl
 
     // Batch fetch
     fun <T> allDocs(options: JSMap<dynamic> = JSMap()): Promise<BulkQueryResult<T>> = noImpl
@@ -65,17 +71,9 @@ open class PouchDB(var name: String, var options: JSMap<dynamic> = JSMap())
     fun info(): Promise<dynamic> = noImpl
 }
 
-@native("Object")
-open class JSObject {
-}
-
-fun jsobject(init: dynamic.() -> Unit): dynamic {
-    val result = JSObject()
-    return result.apply(init)
-}
 
 @native("Object")
-class JSMap<T> {
+open class JSMap<T> {
     @nativeGetter
     operator fun get(key: String): T = noImpl
 
@@ -83,8 +81,15 @@ class JSMap<T> {
     operator fun set(key: String, value: T): Unit = noImpl
 }
 
+open class Object : JSMap<dynamic>() {
+}
+
+fun jsobject(init: dynamic.() -> Unit): dynamic {
+    return (Object()).apply(init)
+}
+
 @native("Object")
-open class PouchDoc() : JSObject() {
+open class PouchDoc() : Object() {
     var _id: String = noImpl
     var type: String = noImpl
 }
@@ -98,29 +103,26 @@ fun <T : PouchDoc> initPouchDoc(doc: T, type: String, _id: String): T {
 
 @native("Object")
 class Node : PouchDoc() {
-    init {
-        type = "Node"
-    }
     var description: String
 
 }
 
 fun node(_id: String): Node {
-    return initPouchDoc(Node(), "Node", _id)
+    return initPouchDoc(Node(), "N", _id)
 }
 
 @native("Object")
 class Edge/*<N>*/ : PouchDoc() {
-    init {
-        type = "Edge"
-    }
     var axis_id: String
     var from: String
     var to: String
 }
 
-fun edge(_id: String): Edge {
-    return initPouchDoc(Edge(), "Edge", _id)
+fun edge(from: Node, to: Node): Edge {
+    return initPouchDoc(Edge(), "E", "f_${from._id}_t_${to._id}").apply {
+        this.from = from._id
+        this.to = to._id
+    }
 }
 
 @native("Object")
@@ -129,26 +131,21 @@ class Axis<E> : PouchDoc() {
 }
 
 fun main(args: Array<String>) {
-//    console.log("Setting email ...")
-//    document.getElementById("email")?.setAttribute("value", "hello@kotlinlang.org")
-
-    val (db, promise) = initDB()
-
-    promise.thenP {
+    initDB().thenP { db ->
         proposeEdges(db)
     }.catchAndLog()
 }
 
 fun proposeEdges(db: PouchDB): Promise<dynamic> {
-    val allPossibleEdges: MutableCollection<Edge> = mutableListOf<Edge>()
+    val allPossibleEdges: MutableCollection<Edge> = mutableListOf()
 
     // Read all nodes and edges
     return db.allDocs<Node>(jsobject {
-        startkey = "Node_"
-        endkey = "Node_\uffff"
+        startkey = "N_"
+        endkey = "N_\uffff"
         include_docs = true
     }).thenV {
-//        console.log(it)
+        console.log(it)
         it.rows.forEach { console.log(it.doc?.description ?: "no desc") }
         it
     }.thenV {
@@ -156,10 +153,7 @@ fun proposeEdges(db: PouchDB): Promise<dynamic> {
         // Find set of all possible edges
         for (from in nodes) {
             for (to in nodes) {
-                allPossibleEdges.add(edge("from_${from._id}_to_${to._id}").apply {
-                    this.from = from._id
-                    this.to = to._id
-                })
+                allPossibleEdges.add(edge(from, to))
             }
         }
         console.log("Possible Edges:")
@@ -168,8 +162,8 @@ fun proposeEdges(db: PouchDB): Promise<dynamic> {
     }.thenP {
         // Remove existing edges
         db.allDocs<Edge>(jsobject {
-            startkey = "Edge_"
-            endkey = "Edge_\uffff"
+            startkey = "E_"
+            endkey = "E_\uffff"
             include_docs = true
         })
     }.thenV {
@@ -184,24 +178,30 @@ fun proposeEdges(db: PouchDB): Promise<dynamic> {
         }
         console.log("Remaining Edges:")
         allPossibleEdges.forEach { console.log(it) }
-    }
 
-    // Return result
+        // Return result
+        allPossibleEdges
+    }
 }
 
 private val DB_NAME = "http://localhost:5984/ranking"
 
-private fun initDB(): Pair<PouchDB, Promise<dynamic>> {
-    val (db, promise) = resetDB()
-    return Pair(db, promise.thenP { addDummyData(db) })
+private fun initDB(): Promise<PouchDB> {
+    return resetDB().thenV { db ->
+        addDummyData(db)
+        db
+    }
 }
 
-private fun resetDB(): Pair<PouchDB, Promise<dynamic>> {
-    (PouchDB(DB_NAME)).destroy()
-    val db: PouchDB = PouchDB(DB_NAME)
+private fun resetDB(): Promise<PouchDB> {
+    return (PouchDB(DB_NAME)).destroy().thenP {
+        val db: PouchDB = PouchDB(DB_NAME)
 
-    val promise = db.info().thenV { console.log(it) }
-    return Pair(db, promise)
+        db.info().thenV {
+            console.log(it)
+            db
+        }
+    }
 }
 
 private fun addDummyData(db: PouchDB): Promise<dynamic> {
@@ -214,17 +214,16 @@ private fun addDummyData(db: PouchDB): Promise<dynamic> {
     var grumpNode = node("grump").apply {
         description = "Grumble to self about difficulty of new stuff";
     }
-    var edge1 = edge("edge1").apply {
+    var edge1 = edge(readNode, sighNode).apply {
         axis_id = "Dependency";
-        from = readNode._id;
-        to = sighNode._id;
     }
-    //TODO 2016-03-29 HughG: Use bulkDocs?  This was an experiment in chaining.
-    return db.put(readNode).thenP {
-        db.put(sighNode)
-    }.thenP {
-        db.put(grumpNode)
-    }.thenP {
-        db.put(edge1)
+    return db.bulkDocs(arrayOf(
+        readNode,
+        sighNode,
+        grumpNode,
+        edge1
+    )).thenV { results ->
+        console.log("Bulk store results:")
+        results.forEach { console.log(it) }
     }
 }
