ac4324de0b7c9919c0a98eb8d23bf44a0380d00e
Hugh Greene <githugh@tameter.org>
2017-04-29 19:01:41 +0100

    Added a TODO about the _rev field.  I'm not sure how to fix that :-(

--- kotlin-test-app/src/org/tameter/kpouchdb/PouchDoc.kt
+++ kotlin-test-app/src/org/tameter/kpouchdb/PouchDoc.kt
@@ -4,6 +4,9 @@ abstract class PouchDoc(
         var _id: String,
         var type: String
 ) {
+    // TODO 2017-04-29 HughG: This should be _rev, but then it's passed as null on new documents, which is rejected
+    // as an illegal format.  Using an empty string doesn't work, either.  Maybe this should be an external class,
+    // with a "constructor" which makes a new empty JS object and sets the properties?
     var rev: String? = null
 
     override fun toString(): String {
