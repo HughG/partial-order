6b4949190820c6c5a0093c9d84445e3028646faa
Phil Tolland <philmwt@hotmail.co.uk>
2017-08-09 11:41:44 +0100

    #20 Request all open issues from Redmine The Redmine version we are using only allows a maximum of 100 issues per request Therefore recursively request 100 issues at a time until no more issues are found

--- partial-order-app/src/main/kotlin/org/tameter/partialorder/source/RedmineSource.kt
+++ partial-order-app/src/main/kotlin/org/tameter/partialorder/source/RedmineSource.kt
@@ -4,10 +4,7 @@ import org.tameter.kotlin.js.jsobject
 import org.tameter.kotlin.js.promise.Promise
 import org.tameter.kpouchdb.PouchDB
 import org.tameter.partialorder.dag.kpouchdb.NodeDoc
-import org.tameter.partialorder.lib.jquery.JQueryAjaxSettings
-import org.tameter.partialorder.lib.jquery.JQueryXHR
-import org.tameter.partialorder.lib.jquery.jQuery
-import org.tameter.partialorder.lib.jquery.toPouchDB
+import org.tameter.partialorder.lib.jquery.*
 import kotlin.js.json
 
 external interface RedmineIssue {
@@ -25,32 +22,59 @@ external interface RedmineSourceSpec {
     val projectId: String?
 }
 
+// By default, a REST query returns 25 pages, and returns only open issues
+// The version of Redmine used allows a maximum of 100 issues per query via the 'limit' keyword
+// Later versions allow the administrator to increase this maximum
+// The 'offset' keyword allows us to skip a number of issues
+// We use this keyword to recursively request until we have all issues
+const val perRedmineRequestLimit = 100
+
 class RedmineSource(val spec: RedmineSourceSpec) : Source {
+
     override fun populate(db: PouchDB): Promise<PouchDB> {
-        var queryURL = "${spec.url}/issues.json"
-        if (spec.projectId != null) {
-            queryURL += "?project_id=${spec.projectId}"
+        return doPopulate(db = db, pageNumber = 0)
+    }
+
+    // Recursively populate database with requests until all pages are found
+    private fun doPopulate(db: PouchDB, pageNumber: Int): Promise<PouchDB> {
+        return makeRequest(pageNumber)
+                .thenP {
+                    db.bulkDocs(it)
+                }.thenP { results ->
+            console.log("Redmine bulk store results, page ${pageNumber}:")
+            results.forEach { console.log(it) }
+            if(results.size < perRedmineRequestLimit) {
+                kotlin.js.Promise.resolve(db)
+            } else {
+                doPopulate(db, pageNumber + 1)
+            }
         }
+    }
+
+    // Perform the Redmine REST API request for a page of open issues, and convert these into NodeDoc format
+    private fun makeRequest(pageNumber: Int): Promise<Array<NodeDoc>> {
         return jQuery.get(jsobject<JQueryAjaxSettings> {
             dataType = "json"
             if (spec.apiKey != null) {
                 headers = json("X-Redmine-API-Key" to spec.apiKey)
             }
-            url = queryURL
+            url = queryUrl(pageNumber)
         }).then({ data: Any, _: String, _: JQueryXHR ->
             val issues = data.unsafeCast<RedmineIssueResponse>().issues
             val issueDocs: Array<NodeDoc> = issues.map {
                 NodeDoc("${spec.url}/issues/${it.id}", it.subject)
             }.toTypedArray()
-            console.log("Bulk store inputs ${queryURL} :")
-            issueDocs.forEach { console.log(it) }
             issueDocs
-        }).toPouchDB().thenP {
-            db.bulkDocs(it)
-        }.thenV { results ->
-            console.log("Bulk store results ${queryURL} :")
-            results.forEach { console.log(it) }
-            db
+        }).toPouchDB()
+    }
+
+    // Form the query URL for the specified results page
+    private fun queryUrl(pageNumber: Int): String {
+        val offset = pageNumber * perRedmineRequestLimit
+        var queryURL = "${spec.url}/issues.json?offset=${offset}&limit=${perRedmineRequestLimit}"
+        if (spec.projectId != null) {
+            queryURL += "&project_id=${spec.projectId}"
         }
+        return queryURL
     }
 }
